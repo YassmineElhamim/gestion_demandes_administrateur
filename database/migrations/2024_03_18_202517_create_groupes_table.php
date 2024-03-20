5b30187168c6f09f97bf395f2b1a9b8a7f6f9de6
YassmineElhamim <yassmine9461@gmail.com>
2024-03-20 09:51:28 +0000

    mmc

---
+++ database/migrations/2024_03_18_202517_create_groupes_table.php
@@ -0,0 +1,33 @@
+  <?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groupes', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("code");
+            $table->enum("annee",array(["1A","2A","3A"]));
+            $table->unsignedBigInteger("filiere_id");
+            $table->timestamps();
+
+            $table->foreign('filiere_id')->references('id')->on('filieres')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groupes');
+    }
+};
---
+++ database/migrations/2024_03_18_203124_create_stagiaires_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stagiaires', function (Blueprint $table) {
+            $table->string('cef')->primary()->unique();
+            $table->unsignedBigInteger('groupe_id');
+            $table->unsignedBigInteger('filiere_id');
+            $table->timestamps();
+
+            $table->foreign('groupe_id')->references('id')->on('groupes')->onDelete('cascade');
+            $table->foreign('filiere_id')->references('id')->on('filieres')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stagiaires');
+    }
+};
